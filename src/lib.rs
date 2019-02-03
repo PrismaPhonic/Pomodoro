b0d30774a241037fe8c0614df2afde456c7efd15
Peter Farr <Peter@PrismaPhonic.com>
2019-02-02 18:24:41 -0800

    Added significant header readme section to src/lib.rs and generated a github README.md file from it

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,43 @@
+//! # pomodoro
+//!
+//! This crate offers you a functional terminal based pomodoro clock.
+//!
+//! # Installation
+//!
+//! This clock requires being built with nightly because of an experimental feature I used to keep
+//! the clock in sync and never fluctuating by more than 1ms.  To install:
+//!
+//! ```terminal
+//! $ cargo +nightly install pomodoro
+//! ```
+//!
+//! # Using pomodoro
+//!
+//! To use, simply run it. By default it will give you a work time of 25 minutes, short break of 5
+//! minutes and a long break of 20 minutes.
+//!
+//! ```terminal
+//! $ pomodoro 
+//! ```
+//!
+//! You can pass it terminal flags to customize the times.  `-w` flag will set the work time, `-s`
+//! will set the short break time, and `-l` will set the long break time.  Here's an example that
+//! sets up a custom pomodoro with 30 minute work time, 10 minute short break and 25 minute long
+//! break:
+//!
+//! ```terminal
+//! $ pomodoro -w 30 -s 10 -l 25
+//! ```
+//!
+//! All of the controls for starting, quitting or resetting a pomodoro are displayed by the
+//! pomodoro menu on launch. `s` will start your next pomodoro. `q` will take you back to the
+//! menu if you are in a pomodoro, or quit if you are at the menu. `r` will reset the current
+//! pomodoro (back to the head of the work cycle and immediately begin countdown).
+//!
+//! Commands are listened for in an asynchronous and non-blocking fashion.
+//!
+//! Enjoy!
+
 #[macro_use]
 extern crate structopt;
 
